5e53f7d10fe26d18acf86b6fc881a416b9cee565
mike <mail.mikejoe@qq.com>
2019-07-29 10:54:41 +0800

    oauth2登录，无状态化

--- web/web-client/src/main/java/com/example/oauth2/web/client/config/ClientConfig.java
+++ web/web-client/src/main/java/com/example/oauth2/web/client/config/ClientConfig.java
@@ -1,4 +1,4 @@
-package com.example.oauth2.web.client;
+package com.example.oauth2.web.client.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
